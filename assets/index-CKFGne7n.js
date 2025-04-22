104491157c34d0fdc44ee5ef2d3138e54a7c14e7
Denis Tsvetkov <denis.tsvetkov59@gmail.com>
2025-04-22 19:08:56 +0500

    Vite config

---
+++ assets/index-CKFGne7n.js
@@ -0,0 +1,1 @@
+import{L as a}from"./features-animation-C6lVa-Dd.js";import"./index-CQ9hs9bL.js";var i=a;export{i as default};
---
+++ assets/src-UW24ZMRV-Du0ih80y.js
@@ -0,0 +1,1 @@
+import{L as a}from"./features-animation-C6lVa-Dd.js";import"./index-CQ9hs9bL.js";var t=a;export{t as default};
